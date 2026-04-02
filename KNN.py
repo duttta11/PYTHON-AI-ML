662325148ba94966e5c49b8305068079965045b4
Aditya Dutta <adityadutta482@gmail.com>
2026-04-02 21:52:51 +0530

    Create KNN.py

--- KNN.py
+++ KNN.py
@@ -22,7 +22,6 @@ df
 X = df[['Gender']]
 y = df[['Disease']]
 X,y
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 knn = KNeighborsClassifier(n_neighbors=3)
