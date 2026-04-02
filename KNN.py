cc69ab1fc303865dce655cd07fa7a47fd981d8ee
Aditya Dutta <adityadutta482@gmail.com>
2026-04-02 21:51:58 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-
-
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
+ from sklearn.model_selection import train_test_split
+ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import LabelEncoder
 
