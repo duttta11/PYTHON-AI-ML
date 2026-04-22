78d7f361f6d3a205aa44437b4ac64f29d882963d
Aditya Dutta <adityadutta482@gmail.com>
2026-04-22 15:20:17 +0530

    Add DecisionTree.py for classification task

---
+++ DecisionTree.py
@@ -0,0 +1,13 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+
+from sklearn.datasets import make_classification
+
+X, y = make_classification(n_samples=5000, n_features=5, n_informative=5, 
+                           n_redundant=0, n_repeated=0, n_classes=3, 
+                           n_clusters_per_class=1, random_state=42)
+
+print(f"Shape of features (X): {X.shape}")
+print(f"Shape of target (y): {y.shape}")
+print(f"Number of classes: {len(set(y))}")
